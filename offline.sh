ddfb49cc7511455ac95412c323b46af720c2dc24
CrazyCat <crazycat69@narod.ru>
2016-08-02 21:38:54 +0300

    Unapply patches before archiving.

--- offline.sh
+++ offline.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+make unapply-patches
 make distclean
 zip -r linux-media-src.zip backports linux v4l Makefile  INSTALL COPYING install.sh -x *.linked_dir -x *.gitignore -x *git_log
 exit
