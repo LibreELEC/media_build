f8946c00fef7bbf0bba2556f705f383c5d80ae11
CrazyCat <crazycat69@narod.ru>
2017-03-14 00:10:52 +0200

    Improved offline archiving script.

--- offline.sh
+++ offline.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-make unapply-patches
-make distclean
+make cleanall
+make dir DIR=./media
 zip -r linux-media-src.zip backports linux v4l Makefile  INSTALL COPYING install.sh -x *.linked_dir -x *.gitignore -x *git_log
 exit
