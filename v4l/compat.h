4cde384d1cac9b1bdcfe05a8f899fb0902b865f6
Matthias Schwarzott <zzam@gentoo.org>
2017-11-04 17:05:06 +0100

    build: Add support for timer_setup
    Literally copied the implementation from kernel
    commit 686fef928bba6be13 (timer: Prepare to change timer callback argument type)
    
    Signed-off-by: Matthias Schwarzott <zzam@gentoo.org>
    Signed-off-by: Hans Verkuil <hans.verkuil@cisco.com>

--- v4l/compat.h
+++ v4l/compat.h
@@ -2195,4 +2195,21 @@ static inline void *bsearch(const void *key, const void *base, size_t num, size_
 }
 #endif
 
+#ifdef NEED_TIMER_SETUP
+#define TIMER_DATA_TYPE                unsigned long
+#define TIMER_FUNC_TYPE                void (*)(TIMER_DATA_TYPE)
+
+static inline void timer_setup(struct timer_list *timer,
+                              void (*callback)(struct timer_list *),
+                              unsigned int flags)
+{
+       __setup_timer(timer, (TIMER_FUNC_TYPE)callback,
+                     (TIMER_DATA_TYPE)timer, flags);
+}
+
+#define from_timer(var, callback_timer, timer_fieldname) \
+       container_of(callback_timer, typeof(*var), timer_fieldname)
+
+#endif
+
 #endif /*  _COMPAT_H */
