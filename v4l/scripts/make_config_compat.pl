b8766c41c6ac225178fbf487f92ac9db912d39f5
Jasmin Jessich <jasmin@anw.at>
2018-03-11 12:41:47 +0100

    Fixed next_pseudo_random32 and __pfn_to_phys support
    Use function check_files_for_func instead of check_files_for_func_uapi.
    
    Signed-off-by: Jasmin Jessich <jasmin@anw.at>

--- v4l/scripts/make_config_compat.pl
+++ v4l/scripts/make_config_compat.pl
@@ -716,14 +716,14 @@ sub check_other_dependencies()
 	check_files_for_func("READ_ONCE", "NEED_READ_ONCE", "include/linux/compiler.h");
 	check_files_for_func("usb_urb_ep_type_check", "NEED_USB_EP_CHECK", "include/linux/usb.h");
 	check_files_for_func("get_user_pages_longterm", "NEED_GET_USER_PAGES_LONGTERM", "include/linux/mm.h");
+	check_files_for_func("__pfn_to_phys", "NEED_PFN_TO_PHYS", "include/asm-generic/memory_model.h");
+	check_files_for_func("next_pseudo_random32", "NEED_NEXT_PSEUDO_RANDOM32", "include/linux/random.h");
 
 	# For tests for uapi-dependent logic
 	check_files_for_func_uapi("usb_endpoint_maxp", "NEED_USB_ENDPOINT_MAXP", "usb/ch9.h");
 	check_files_for_func_uapi("usb_endpoint_maxp_mult", "NEED_USB_ENDPOINT_MAXP_MULT", "usb/ch9.h");
 	check_files_for_func_uapi("PCI_EXP_DEVCTL2_COMP_TIMEOUT", "NEED_PCI_EXP_DEVCTL2_COMP_TIMEOUT", "pci_regs.h");
 	check_files_for_func_uapi("__poll_t", "NEED_POLL_T", "types.h");
-	check_files_for_func_uapi("__pfn_to_phys", "NEED_PFN_TO_PHYS", "include/asm-generic/memory_model.h");
-	check_files_for_func_uapi("next_pseudo_random32", "NEED_NEXT_PSEUDO_RANDOM32", "include/linux/random.h");
 }
 
 # Do the basic rules
