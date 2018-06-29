31154793f582214049d8b1985af3c82a1b1373ed
CrazyCat <crazycat69@narod.ru>
2018-06-29 23:38:44 +0300

    install.sh: Remove media modules in wrong place after old linux-tbs-drivers.

--- install.sh
+++ install.sh
@@ -9,6 +9,7 @@ make -j5
 echo "V4L drivers installing..."
 sudo rm -r -f /lib/modules/$(uname -r)/kernel/drivers/media
 sudo rm -r -f /lib/modules/$(uname -r)/kernel/drivers/staging/media
+sudo rm -r -f /lib/modules/$(uname -r)/kernel/drivers/linux
 sudo rm -r -f /lib/modules/$(uname -r)/extra
 
 
