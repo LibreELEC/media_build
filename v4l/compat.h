0f889215b2a26641f02ed07b8880091476836157
Hans Verkuil <hverkuil-cisco@xs4all.nl>
2020-12-03 08:32:34 +0100

    Add compat code for dev_err_probe()
    Signed-off-by: Hans Verkuil <hverkuil-cisco@xs4all.nl>

--- v4l/compat.h
+++ v4l/compat.h
@@ -2941,4 +2941,25 @@ static inline bool in_compat_syscall(void) { return is_compat_task(); }
 
 #endif
 
+#ifdef NEED_DEV_ERR_PROBE
+static inline int dev_err_probe(const struct device *dev, int err, const char *fmt, ...)
+{
+        struct va_format vaf;
+        va_list args;
+
+        va_start(args, fmt);
+        vaf.fmt = fmt;
+        vaf.va = &args;
+
+        if (err != -EPROBE_DEFER)
+                dev_err(dev, "error %pe: %pV", ERR_PTR(err), &vaf);
+        else
+                dev_dbg(dev, "error %pe: %pV", ERR_PTR(err), &vaf);
+
+        va_end(args);
+
+        return err;
+}
+#endif
+
 #endif /*  _COMPAT_H */
